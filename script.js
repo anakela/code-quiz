700ef6df37a82b3f2a792e02fb4dcbd3613949ea
Angie <103882157+anakela@users.noreply.github.com>
2022-05-21 14:10:17 -1000

    Updated JS and README.md

--- script.js
+++ script.js
@@ -1,26 +1,87 @@
 // GIVEN I am taking a code quiz - TIMER FOR EACH QUESTION NOT NECESSARY; CAN BE FOR ENTIRE QUIZ
 
-// ADD QUESTIONS HERE (Array of objects)
-const question1 = 
+// An array of objects containing questions, answers, and the correct answers.
+const questionArray = [
+    {
+        Q: "TestingQ1",
+        A: [
+            "Testing2",
+            "Testing3",
+            "Testing4",
+            "Testing5"
+        ],
+        Correct: "Testing2"
+    },
+    {
+        Q: "TestingQ2",
+        A: [
+            "Testing2",
+            "Testing3",
+            "Testing4",
+            "Testing5"
+        ],
+        Correct: "Correct answer here."
+    },
+    {
+        Q: "TestingQ3",
+        A: [
+            "Testing2",
+            "Testing3",
+            "Testing4",
+            "Testing5"
+        ],
+        Correct: "Correct answer here."
+    },
+    {
+        Q: "TestingQ4",
+        A: [
+            "Testing2",
+            "Testing3",
+            "Testing4",
+            "Testing5"
+        ],
+        Correct: "Correct answer here."
+    },
+    {
+        Q: "TestingQ5",
+        A: [
+            "Testing2",
+            "Testing3",
+            "Testing4",
+            "Testing5"
+        ],
+        Correct: "Correct answer here."
+    }
 
-// WHEN I click the start button
-// Add an event listener here.
+];
+
+// Set to zero to target the first index in an array of objects.
+let questionIndex = 0;
+
+// startButton is equal to elements with the "start" class.  Here, button.
 let startButton = document.querySelector(".start");
+// timeLeft is equal to elements with the "time=left" class.  Here, the h2 tag.
 let timeLeft = document.querySelector(".time-left");
+// showQuestions is equal to elements with the "show-questions" class.  Here, a section.
+let showQuestions = document.querySelector(".show-questions");
+// showAnswers is equal to the elements in the "show-answers" class.  Here, a section.
+let showAnswers = document.querySelector(".show-answers");
 
-// Global variable for time (100 seconds)
+let results = document.querySelector("#results");
+
+// Global variable for time (100 seconds).
 let secondsLeft = 100;
-// Set this variable to 0 so that it targets the first index in an array of objects
-let indexTracking = 0;
 
-// When clicking the start button, start the countdown!
+// The start button being clicked begins the countDown and displayQuestions functions.
 startButton.addEventListener("click", function () {
     countDown();
+    displayQuestions();
     // Hide the start button
-    startButton.style.display="none";
+    startButton.style.display = "none";
 });
 
 // Create a function that starts the timer
+// The countDown function sets a timer that ticks down once per second until the seconds reach 0.
 function countDown() {
     let timer = setInterval(function () {
         secondsLeft--;
@@ -32,19 +93,47 @@ function countDown() {
             // If the seconds left are <= 0, show this message:
             timeLeft.textContent = "Sorry, there's no time left!";
             // Let the user know that the game is over.
-            alert("Sorry, time's up!  Let's see your score.");
+            // alert("Sorry, time's up!  Let's see your score.");
             // Execute in-game function to show score and type in initials.
         }
 
     }, 1000);
 }
 
-
 // and I am presented with a question
 // Create a function that displays questions
 function displayQuestions() {
-    // Check answer, subtract or add points, then update the indexTracking by 1.
+    showQuestions.textContent = questionArray[questionIndex].Q;
+    // console.log(showAnswers);
+    for (let i = 0; i < questionArray[questionIndex].A.length; i++) {
+        let answerButton = document.createElement("button");
+        answerButton.textContent = questionArray[questionIndex].A[i];
+        // Define the function here to check the answers
+        answerButton.onclick = function checkAnswers() {
+            // If the submission is equal to Correct...
+            if (answerButton.innerText === questionArray[questionIndex].Correct) {
+                results.textContent = "Right on, popcorn! That's correct.";
+                console.log(checkAnswers);
+                displayQuestions();
+            } else {
+                results.textContent = "Sorry, no such luck. Try again!";
+                secondsLeft -= 10;
+            }
+        };
+        // displayQuestions();
+        // console.log(answerButton);
+        showAnswers.appendChild(answerButton);
+        console.log();
+    }
+
+    // showAnswers.textContent = questionArray[questionIndex].A;
+    // showAnswers.append = questionArray[questionIndex].B;
+    // showAnswers.append = questionArray[questionIndex].C;
+    // showAnswers.append = questionArray[questionIndex].D;
+
 }
+    // Check answer, subtract or add points, then update the indexTracking by 1.
+
 
 // WHEN I answer a question
 // Add a variable for user input (multiple choice: buttons, radios, etc.?)
